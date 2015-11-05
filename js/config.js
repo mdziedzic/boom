a6006ed656c238baa2643faa159a78f706f7150b
Michael Dziedzic <mdz@eggfoo.com>
2015-11-05 11:23:25 -0800

    configuration edit

--- js/config.js
+++ js/config.js
@@ -3,9 +3,9 @@
 var BOOM = BOOM || {};
 
 BOOM.config = {
-	numberOfHTiles: [10, 20, 50, 100, 1],
-	numberOfVTiles: [10, 20, 50, 100, 300],
-	bombPercent:  [10, 10, 10, 10, 10],
+	numberOfHTiles: [10, 20, 50, 70, 1],
+	numberOfVTiles: [10, 20, 50, 70, 300],
+	bombPercent:  [10, 10, 20, 50, 10],
 	tileSize:  [50, 25, 25, 20, 50],
-	animSpeed: [10, 3, 1, 0, 10]
+	animSpeed: [10, 3, 1, 1, 10]
 };
